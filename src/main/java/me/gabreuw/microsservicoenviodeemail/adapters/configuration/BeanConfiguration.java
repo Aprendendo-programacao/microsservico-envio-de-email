1d6135ec421ccd15f723a8e06ad8f0097767799a
ImGabreuw <imgabreuw.dev@gmail.com>
2021-09-05 18:05:03 -0300

    Adição e configuração do ModelMapper

--- src/main/java/me/gabreuw/microsservicoenviodeemail/adapters/configuration/BeanConfiguration.java
+++ src/main/java/me/gabreuw/microsservicoenviodeemail/adapters/configuration/BeanConfiguration.java
@@ -1,20 +1,29 @@
 package me.gabreuw.microsservicoenviodeemail.adapters.configuration;
 
 import me.gabreuw.microsservicoenviodeemail.MicrosservicoEnvioDeEmailApplication;
-import me.gabreuw.microsservicoenviodeemail.application.ports.EmailRepository;
+import me.gabreuw.microsservicoenviodeemail.application.ports.EmailRepositoryPort;
+import me.gabreuw.microsservicoenviodeemail.application.ports.SendEmailServicePort;
 import me.gabreuw.microsservicoenviodeemail.application.services.EmailServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 @ComponentScan(basePackageClasses = MicrosservicoEnvioDeEmailApplication.class)
 public class BeanConfiguration {
 
     @Bean
-    public EmailServiceImpl emailServiceImpl(EmailRepository repository, JavaMailSender emailSender) {
-        return new EmailServiceImpl(repository, emailSender);
+    public EmailServiceImpl emailServiceImpl(
+            EmailRepositoryPort repository,
+            SendEmailServicePort sendEmailServicePort
+    ) {
+        return new EmailServiceImpl(repository, sendEmailServicePort);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
